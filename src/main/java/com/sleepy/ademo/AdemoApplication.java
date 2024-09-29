22533452d24ef8e57da0142d2405b974b1d469bf
‘sleeyzhang’ <zhangmingan111@163.com>
2024-09-29 14:57:02 +0800

    init

---
+++ src/main/java/com/sleepy/ademo/AdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sleepy.ademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sleepy/ademo/AdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepy.ademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
